45566d59e99d77e1aba08ecaa3427f9f446f6040
MinaSaad47 <MinaSaad-47@outlook.com>
2021-08-29 03:30:32 +0200

    initial permission request (beta)

--- app/src/main/java/com/example/marketmanagementsystem/ScannerActivity.java
+++ app/src/main/java/com/example/marketmanagementsystem/ScannerActivity.java
@@ -2,9 +2,14 @@ package com.example.marketmanagementsystem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -13,6 +18,8 @@ import com.budiyev.android.codescanner.CodeScanner;
 import com.budiyev.android.codescanner.CodeScannerView;
 import com.budiyev.android.codescanner.DecodeCallback;
 import com.google.zxing.Result;
+
+import java.util.ArrayList;
 
 public class ScannerActivity extends AppCompatActivity {
 
@@ -23,6 +30,12 @@ public class ScannerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[] {Manifest.permission.CAMERA}, 11);
+        }
 
         codeScannerView = findViewById(R.id.scanner_view);
         codeScanner = new CodeScanner(ScannerActivity.this, codeScannerView);
